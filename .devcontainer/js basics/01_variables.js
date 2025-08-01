73c8a63a5ba5a40fc91f990dab7974840d9e1920
SUFIYAN KHAN <141114439+SUFI205@users.noreply.github.com>
2025-08-01 16:31:37 +0000

    container config done

---
+++ .devcontainer/js basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountid=144553
+let accountemail="john.doe@example.com"
+let accountpassword="password123"
+accountcity="New York"
+let accountcountry;
+//accountid=2
+
+/*
+prefer not to use var 
+*/
+
+accountemail="jane.doe@example.com" 
+accountpassword="newpassword456"
+accountcity="Los Angeles"
+
+console.log(accountid);
+
+console.table([accountid, accountemail, accountpassword, accountcity, accountcountry]); 
