f760a1128b45f11a61f5f5a6f558c44425ae393f
SUFIYAN KHAN <141114439+SUFI205@users.noreply.github.com>
2025-08-02 04:46:53 +0000

    comparision js

---
+++ .devcontainer/js basics/04_compariosion.js
@@ -0,0 +1,35 @@
+console.log(2>1);
+console.log(2<1);
+console.log(2>=2);
+console.log(2<=2);
+console.log(2==2);
+console.log(2!=3);
+console.log(2===2);
+console.log(2!==3);
+
+
+console.log("2">1);
+console.log("02"<1);
+
+console.log(null>0);
+console.log(null==0);
+console.log(null===0);
+
+console.log(undefined>0);
+console.log(undefined==0);
+console.log(undefined===0);
+
+console.log(NaN>0);
+console.log(NaN==0);
+console.log(NaN===0);
+
+let value1 = 3;
+let value2 = 3;
+console.log(value1 === value2); // true
+console.log(value1 !== value2); // false
+
+///===
+console.log("2" == 2);
+ // true
+
+ 
