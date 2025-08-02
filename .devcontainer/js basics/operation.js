94dcd9ba0a5a17ac09eb01c057259bc323fe29b8
SUFIYAN KHAN <141114439+SUFI205@users.noreply.github.com>
2025-08-02 04:35:48 +0000

    operations

---
+++ .devcontainer/js basics/operation.js
@@ -0,0 +1,29 @@
+let value=3
+let negativeValue=-value;
+console.log(negativeValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2%2);
+
+let str1="hello";
+let str2="world";
+console.log(str1+str2);
+console.log(str1+" "+str2);
+
+console.log("2" + 2); // string concatenation
+console.log("2" - 2); // numeric subtraction
+console.log(1+2+"2"); // numeric multiplication
+console.log("1"+"2"+2);
+
+console.log(+true);
+console.log(+"");
+
+let num1,num2,num3;
+num1=num2=num3=2+2;
+
+let gamecounter =100;
+gamecounter++;
+console.log(gamecounter);
