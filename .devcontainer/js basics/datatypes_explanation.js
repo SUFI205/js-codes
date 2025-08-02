4abca9a903cf7b672360be1d61bd35afe8a22fbe
SUFIYAN KHAN <141114439+SUFI205@users.noreply.github.com>
2025-08-02 05:13:28 +0000

    datatype exp

---
+++ .devcontainer/js basics/datatypes_explanation.js
@@ -0,0 +1,18 @@
+const { useInsertionEffect } = require("react");
+
+let myyoutubechannel="My YouTube Channel";
+let anothername="dewfbfib";
+anothername="dewfbfib";
+
+console.log(myyoutubechannel);
+console.log(anothername);
+
+let user=
+{
+    email:"example@example.com",
+    upi:"example@upii"
+}
+
+let usertwo=userone;
+usertwo.email="hites@example.com";
+console.log(user.email); 
