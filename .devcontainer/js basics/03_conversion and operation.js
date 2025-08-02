702025b80195c835d6815486563ba3c808de5c14
SUFIYAN KHAN <141114439+SUFI205@users.noreply.github.com>
2025-08-02 04:21:53 +0000

    conversion of datatypes

---
+++ .devcontainer/js basics/03_conversion and operation.js
@@ -0,0 +1,12 @@
+let score="100";
+
+console.log(score);
+console.log(typeof score);
+
+
+let valinnumber=Number(score);
+console.log(typeof valinnumber);
+console.log(valinnumber);
+let loggedin=1
+let isloggedin=Boolean(loggedin);
+console.log(isloggedin);
