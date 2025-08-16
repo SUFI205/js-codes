1a90ecdf397df8886e76338c3ed73e2889b1816b
SUFIYAN KHAN <141114439+SUFI205@users.noreply.github.com>
2025-08-16 17:20:07 +0000

    objects

---
+++ .devcontainer/js basics/09_objects.js
@@ -0,0 +1,14 @@
+//singleton
+
+//object literals
+const jsuser={
+    name: "hitesh",
+    age: 30,
+    location: "India",
+    isActive: true,
+    email: "hitesh@example.com",
+    isloggedin: false,
+    lastlogin: ["monday","saturday"]
+}
+
+console.log(jsuser);
