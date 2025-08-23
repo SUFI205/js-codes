20f67b81eb74dfb74358e7d4f533e506d363dc21
SUFIYAN KHAN <141114439+SUFI205@users.noreply.github.com>
2025-08-23 05:54:51 +0000

    13_scope_mini

---
+++ .devcontainer/js basics/13_scope_mini hosting.js
@@ -0,0 +1,42 @@
+function one()
+{
+    const username="John"
+
+    function two()
+    {
+        const websites="youtube"
+        console.log(username)
+        console.log(websites) 
+    }
+
+    two()
+}
+
+one()
+
+if(true)
+{
+    const username="john"
+    if(username==="john")
+    {
+        const websites="youtube"
+        console.log(username+websites);
+    }
+    //console.log(websites);
+}
+
+//console.log(username);
+
+function addone(num)
+{
+    return num+1
+}
+
+console.log(addone(5));
+
+const addtwo=function(num)
+{
+    return num+2
+}
+
+addtwo(5)
