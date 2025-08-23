c2c79cc32fc3084c8a2f5ca0f34da4ff6f1c617c
SUFIYAN KHAN <141114439+SUFI205@users.noreply.github.com>
2025-08-23 05:25:18 +0000

    function2_js

---
+++ .devcontainer/js basics/11_function2.js
@@ -0,0 +1,29 @@
+function calculatecartprice(...num1)
+{
+    return num1;
+}
+
+console.log(calculatecartprice(100,200,300,400,500,600,700,800,900));
+
+
+const user={
+    username: "sufiyan",
+    price:199
+}
+
+function handleobject(user)
+{
+    console.log('username is ${user.uesrname} and price is ${user.price}');
+
+}
+handleobject(user);
+
+const mynewarr=[200,400,100,600]
+function handlearray(mynewarr)
+{
+    return getArray[1]
+}
+
+console.log(handlearray(mynewarr));
+
+
