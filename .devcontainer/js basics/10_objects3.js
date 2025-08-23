a9a66b93d9c36f5c7a1fe41711b0fee3eb820b03
SUFIYAN KHAN <141114439+SUFI205@users.noreply.github.com>
2025-08-23 04:47:05 +0000

    object 3

---
+++ .devcontainer/js basics/10_objects3.js
@@ -0,0 +1,11 @@
+//destructuring
+const course={
+    coursename:"js in english",
+    price:999,
+    courseinstructor:"sachin sir"
+}
+
+const {courseinstructor}=course;
+
+console.log(courseinstructor);
+
